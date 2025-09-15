7733ad8a994a31165e181006c12e66451cfd6cce
Ali.Mahdavi <84a.mahdaviniya@gmail.com>
2025-09-15 11:20:01 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,3 +31,36 @@ setInterval(()=>{
     if(slideIndex>5) slideIndex=1;
     setSlide(`slide${slideIndex}`,slideIndex)
 },4000)
+
+document.querySelectorAll(".product-slider").forEach(slider => {
+  const imgs = slider.querySelectorAll("img");
+  const prev = slider.querySelector(".prev");
+  const next = slider.querySelector(".next");
+  let index = 0;
+
+  function showImage(i) {
+    imgs.forEach(img => img.classList.remove("active"));
+    imgs[i].classList.add("active");
+  }
+
+  prev.addEventListener("click", () => {
+    index = (index - 1 + imgs.length) % imgs.length;
+    showImage(index);
+  });
+
+  next.addEventListener("click", () => {
+    index = (index + 1) % imgs.length;
+    showImage(index);
+  });
+});
+
+document.querySelectorAll(".shopping-card .img-sec .stars").forEach(starContainer => {
+  const rating = parseInt(starContainer.getAttribute("data-rating"));
+  const stars = starContainer.querySelectorAll("i");
+
+  stars.forEach((star, index) => {
+    if (index < rating) {
+      star.classList.add("active");
+    }
+  });
+});
