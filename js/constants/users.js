eea8534089d6f5937b52f89168448e9eeaf084d6
Ali.Mahdavi <84a.mahdaviniya@gmail.com>
2025-10-21 12:11:13 +0330

    Delete js directory

--- js/constants/users.js
+++
@@ -1,7 +0,0 @@
-export const users = [
-    {username: "Ali" , password: "ali"},
-    {username: "Asal" , password: "asal"},
-    {username: "Arian" , password: "arian"}
-];
-
-export default users;
