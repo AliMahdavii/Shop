da7ddc0cc58b39f9238eb2711e36cf946b6eeca4
Ali.Mahdavi <84a.mahdaviniya@gmail.com>
2025-11-09 11:56:49 +0330

    Add files via upload

---
+++ js/constants/otherProducts.js
@@ -0,0 +1,94 @@
+export const otherProducts = [
+    {
+        name: "ژاکت ‌Blevin",
+        price: 1800000,
+        mainImage: "images/6/IMG_1936.AVIF",
+        gallery: [
+            "images/6/IMG_1937.AVIF",
+            "images/6/IMG_1938.AVIF"
+        ],
+        category: "ژاکت",
+        description : "ژاکتی نرم و گرم از الیاف باکیفیت، ایده‌آل برای روزهای خنک پاییز و زمستان. راحت، ساده و همیشه شیک.",
+        stars: 2,
+        isHotOffer: true,
+        id: "6"
+    },
+    {
+        name: "شلوار Brandon",
+        price: 2100000,
+        mainImage: "images/7/IMG_1940.AVIF",
+        gallery: [
+            "images/7/IMG_1940.AVIF",
+            "images/7/IMG_1941.AVIF",
+            "images/7/IMG_1942.AVIF",
+            "images/7/IMG_1943.AVIF"
+        ],
+        category: "شلوار",
+        description : "شلواری راحت و خوش‌دوخت، مناسب استفاده روزمره و استایل خیابانی. ترکیب عالی از سادگی و راحتی برای هر موقعیتی.",
+        stars: 4,
+        isHotOffer: false,
+        id: "7"
+    },
+    {
+        name: "کوله پشتی Kickflip",
+        price: 1960000,
+        mainImage: "images/8/IMG_1964.AVIF",
+        gallery: [
+            "images/8/IMG_1965.AVIF",
+            "images/8/IMG_1966.AVIF"
+        ],
+        category: "کوله پشتی",
+        description : "کوله‌پشتی جادار و بادوام با طراحی کاربردی. مناسب دانشگاه، سفر یا استفاده روزمره با دوخت مستحکم و ظاهر مدرن.",
+        stars: 3,
+        isHotOffer: false,
+        id: "8"
+    },
+    {
+        name: "کلاه Carhatt",
+        price: 710000,
+        mainImage: "images/9/IMG_1961.AVIF",
+        gallery: [
+            "images/9/IMG_1962.AVIF"
+        ],
+        category: "کلاه",
+        description : "کلاهی سبک و راحت با جنس باکیفیت، مناسب روزهای آفتابی یا تکمیل استایل اسپرت. هم کاربردی هم فشن.",
+        stars: 3,
+        isHotOffer: false,
+        id: "9"
+    },
+    {
+        name: "شلوارک Rylan",
+        price: 1200000,
+        mainImage: "images/10/IMG_1955.AVIF",
+        gallery: [
+            "images/10/IMG_1956.AVIF",
+            "images/10/IMG_1957.AVIF",
+            "images/10/IMG_1958.AVIF",
+            "images/10/IMG_1959.AVIF"
+        ],
+        category: "شلوارک",
+        description : "شلوارکی خنک و سبک، مناسب روزهای گرم تابستان و فعالیت‌های روزمره. راحتی همراه با استایل مینیمال.",
+        stars: 3,
+        isHotOffer: false,
+        id: "10"
+    },
+    {
+        name: " کیف دستی Bayfield",
+        price: 800000,
+        mainImage: "images/11/IMG_1945.AVIF",
+        gallery: [
+            "images/11/IMG_1946.AVIF",
+            "images/11/IMG_1947.AVIF",
+            "images/11/IMG_1948.AVIF",
+            "images/11/IMG_1949.AVIF"
+        ],
+        category: "کیف دستی ",
+        description : "کیف دستی ظریف با طراحی مینیمال و دوخت دقیق، مناسب برای استفاده روزمره یا موقعیت‌های خاص. سبک و در عین حال جادار.",
+        stars: 3,
+        isHotOffer: true,
+        id: "11"
+    }
+
+];
+
+export default otherProducts;
