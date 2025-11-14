14fc2e4236eaf5a224fc31cbe1d2e1fcd55487b7
Ali.Mahdavi <84a.mahdaviniya@gmail.com>
2025-11-14 12:31:03 +0330

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loggedInUser = JSON.parse(localStorage.getItem("logged-in"))?.username;
+  const cartContainer = document.querySelector(".cart-container");
+  const cartItemContainer = document.querySelector("#cart-items-container");
+  const totalItems = document.querySelector("#total-items");
+  const totalPrice = document.querySelector("#total-price");
+  const usernameLabel = document.querySelector("#cart-username");
+
+  if (!loggedInUser) {
+    cartContainer.innerHTML = `
+      <h1>ابتدا وارد حساب خود شوید</h1>
+      <a href="login.html" class="back-btn">ورود</a>
+    `;
+    return;
+  }
+
+  usernameLabel.textContent = `کاربر: ${loggedInUser}`;
+
+  const cart = JSON.parse(localStorage.getItem(loggedInUser))?.cart || [];
+
+  if (cart.length === 0) {
+    cartItemContainer.innerHTML = `<p class="empty-p">سبد خرید شما خالی است 🛒</p>`;
+    return;
+  }
+
+  let total = 0;
+
+  cart.forEach(({ name, price, id, mainImage }) => {
+    total += price;
+    cartItemContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="shopping-card" onclick="openProduct('${id}')">
+        <img src="${mainImage}" alt="${name}">
+        <div class="title">${name}</div>
+        <div class="buttons">
+          <span class="price">${price.toLocaleString()} تومان</span>
+          <button onclick="event.stopPropagation(), deleteItem(${id})" class="deletItem">حذف</button>
+        </div>
+      </div>
+      `
+    );
+  });
+
+  totalItems.textContent = `تعداد کالاها: ${cart.length}`;
+  totalPrice.textContent = `مجموع کل: ${total.toLocaleString()} تومان`;
+});
+
+function deleteItem(id) {
+  const username = JSON.parse(localStorage.getItem("logged-in"))?.username;
+
+  if (!username) {
+    console.error("کاربر وارد نشده!");
+    return;
+  }
+
+  let userData = JSON.parse(localStorage.getItem(username)) || { cart: [] };
+
+  if (!Array.isArray(userData.cart)) {
+    userData.cart = [];
+  }
+
+  userData.cart = userData.cart.filter(item => item.id != id);
+
+  localStorage.setItem(username, JSON.stringify(userData));
+
+  location.reload();
+}
+
+
+function openProduct(id){
+        window.location.href = `product.html?id=${id}`;
+    }
---
+++ js/main.js
@@ -0,0 +1,251 @@
+let slideIndex=1;
+let remainingTime=70000;
+
+function setTime(){
+    if(remainingTime==0) return;
+    let h = Math.floor(remainingTime/3600);
+    let m = Math.floor((remainingTime%3600)/60);
+    let s = (remainingTime%3600)%60;
+    document.querySelector('#hours').innerHTML = h;
+    document.querySelector('#minutes').innerHTML = m;
+    document.querySelector('#seconds').innerHTML = s;
+}
+
+setInterval(()=>{
+    remainingTime--;
+    setTime()
+},1000)
+
+function setSlide(input,index){
+    slideIndex=index;
+    let item=document.querySelector(`#${input}`)
+    let slides=[...document.querySelector('.slides').children];
+    slides.forEach((element)=>{
+        element.classList.remove('active');
+    })
+    item.classList.add('active');
+}
+
+setInterval(()=>{
+    slideIndex++;
+    if(slideIndex>5) slideIndex=1;
+    setSlide(`slide${slideIndex}`,slideIndex)
+},4000);
+
+
+import products from "./constants/product.js";
+import superOfferProducts from "./constants/supperOfferProduct.js";
+import otherProducts from "./constants/otherProducts.js";
+
+const doesUserLoggedIn = JSON.parse(
+  localStorage.getItem("logged-in")
+)?.username;
+
+const headerDynamicContentContainer = document.querySelector("#header-dynamic-content");
+const productSecNew = document.querySelector(".product-sec-new");
+const supperOfferSection = document.querySelector(".supper-offer-section");
+const otherClothes = document.querySelector(".other-clothes");
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    if(doesUserLoggedIn){
+
+        const cartData = JSON.parse(localStorage.getItem(doesUserLoggedIn)) || {};
+        const cart = cartData.cart || [];
+
+        headerDynamicContentContainer.insertAdjacentHTML(
+            "beforeend",
+                `<div class="nav-right">
+            <a href="profile.html"><i class="fa-solid fa-user"></i></a>
+            <a href="cart.html"><i class="fa-solid fa-cart-shopping">
+                <span id="cart-badge">${cart.length}</span>
+            </i></a>
+            <a href="index.html"><i class="fa-solid fa-house"></i></a>
+            <a href="javascript:void(0)" id="search-btn"><i class="fa-solid fa-magnifying-glass"></i></a>
+            <input type="text" id="search-box" placeholder="جستجو..." style="display:none;">
+                </div>
+        <div class="nav-left">
+            <div class="register-login-btn" onclick="logout()" id="logout-button">
+                <i class="fa-solid fa-right-from-bracket"></i>
+                <a href=""> خروج </a>
+            </div>
+        </div>`
+        );
+
+        const searchBtn = document.getElementById("search-btn");
+        const searchBox = document.getElementById("search-box");
+
+        searchBtn.addEventListener("click", () => {
+
+            if (searchBox.style.display === "none" || searchBox.style.display === "") {
+            searchBox.style.display = "inline-block";
+            searchBox.focus();
+          } else {
+
+            const query = searchBox.value.trim();
+            if (query) {
+              window.location.href = `search.html?q=${encodeURIComponent(query)}`;
+            } else {
+
+                searchBox.style.display = "none";
+            }
+          }
+    });
+
+  searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const query = searchBox.value.trim();
+      if (query) {
+        window.location.href = `search.html?q=${encodeURIComponent(query)}`;
+      }
+    }
+  });
+
+    } else{
+        headerDynamicContentContainer.insertAdjacentHTML(
+            "beforeend",
+            `<div class="nav-right">
+            <a href="login.html"><i class="fa-solid fa-user"></i></a>
+            <a href="cart.html"><i class="fa-solid fa-cart-shopping"></i></a>
+            <a href="index.html"><i class="fa-solid fa-house"></i></a>
+            <a href="javascript:void(0)" id="search-btn"><i class="fa-solid fa-magnifying-glass"></i></a>
+            <input type="text" id="search-box" placeholder="جستجو..." style="display:none;">
+                </div>
+        <div class="nav-left">
+            <div class="register-login-btn">
+                <i class="fa-solid fa-user"></i>
+                <a href="login.html">ثبت نام / ورود</a>
+            </div>
+        </div>`
+        );
+
+       document.getElementById("search-btn").addEventListener("click", () => {
+            const searchBox = document.getElementById("search-box");
+            searchBox.style.display = searchBox.style.display === "none" ? "inline-block" : "none";
+        if (searchBox.style.display === "inline-block") searchBox.focus();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loggedInUser = JSON.parse(localStorage.getItem("logged-in"))?.username;
+    if (!loggedInUser) return;
+
+    const favorites = JSON.parse(localStorage.getItem(loggedInUser))?.favorites || [];
+
+    favorites.forEach(fav => {
+        const favElement = document.querySelector(`.favorite-product[data-id="${fav.id}"]`);
+        if (favElement) {
+            favElement.classList.add("active");
+            favElement.querySelector("i").classList.remove("far");
+            favElement.querySelector("i").classList.add("fas");
+        }
+    });
+});
+
+    }
+
+    products.map((product) => {
+            productSecNew.insertAdjacentHTML(
+                "beforeend",
+                `<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+                    <div class="shopping-card" onclick="openProduct('${product?.id}')">
+                    <div class="img-sec">
+                        <div class="product-slider">
+                            <img src="${product?.mainImage}" alt="" class="active">
+                            ${product?.gallery.map(img => `<img src="${img}" alt="">`).join('')}
+                        </div>
+                        ${product.isHotOffer?`<span class="hot-offer">پیشنهاد ویژه</span>`:""}
+                    </div>
+                    <div class="title">
+                        ${product?.name}
+                    </div>
+                    <div class="buttons">
+                            <span class="price">${product?.price.toLocaleString()} تومان</span>
+                    </div>
+                </div>
+                </div>`);
+        });
+
+        superOfferProducts.map((superOfferProduct) => {
+            supperOfferSection.insertAdjacentHTML(
+                "beforeend",
+                `<div class="row">
+                <div class="col-lg-9">
+                    <div class="supper-offer-box" onclick="openProduct('${superOfferProduct?.id}')">
+                        <span class="supper-offer-box-title"> پیشنهاد شگفت انگیز </span>
+                        <div class="body">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="image-container">
+                                        <div class="product-slider">
+                            <img src="${superOfferProduct?.mainImage}" alt="" class="active">
+                            ${superOfferProduct?.gallery.map(img => `<img src="${img}" alt="">`).join('')}
+                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-8 info">
+                                    <div class="spesificiation">
+                                        <div class="title">${superOfferProduct?.name}</div>
+                                        <div class="price-row">
+                                            <div class="price"> ${superOfferProduct?.price.toLocaleString()} تومان </div>
+                                            <div class="discount-price"> ${superOfferProduct?.discountPrice.toLocaleString()} تومان </div>
+                                            <div class="off"> ${superOfferProduct.percent}% تخفیف </div>
+                                        </div>
+                                        <div class="counter-down">
+                                            <span id="seconds">55</span> :
+                                            <span id="minutes">19</span> :
+                                            <span id="hours">15</span>
+                                        </div>
+                                        <div class="remaining-time"> زمان باقی مانده تا پایان سفارش </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="banner">
+                        <img src="images/IMG_1927.JPG" alt="">
+                    </div>
+                </div>
+            </div>`
+            );
+        });
+
+        otherProducts.map((otherProduct) => {
+            
+            otherClothes.insertAdjacentHTML(
+                "beforeend",
+                `<div class="item">
+                    <div class="shopping-card" onclick="openProduct('${otherProduct?.id}')">
+                    <div class="img-sec">
+                        <div class="product-slider">
+                            <img src="${otherProduct?.mainImage}" alt="" class="active">
+                            ${otherProduct?.gallery.map(img => `<img src="${img}" alt="">`).join('')}
+                        </div>
+                        ${otherProduct.isHotOffer?`<span class="hot-offer">پیشنهاد ویژه</span>`:""}
+                    </div>
+                    <div class="title">
+                        ${otherProduct?.name}
+                    </div>
+                    <div class="buttons">
+                            <span class="price">${otherProduct?.price.toLocaleString()} تومان</span>
+                    </div>
+                </div>
+                </div>`
+            );
+        });
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loggedInUser = JSON.parse(localStorage.getItem("logged-in"))?.username;
+    if (!loggedInUser) return;
+
+    const favorites = JSON.parse(localStorage.getItem(loggedInUser))?.favorites || [];
+
+    favorites.forEach(fav => {
+        const favElement = document.querySelector(`.favorite-product[data-id="${fav.id}"]`);
+        if (favElement) {
+            favElement.classList.add("active");
+        }
+    });
+});
